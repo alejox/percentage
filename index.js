ca0c40fd2f99661b67e6b6c1425ca1560446e6f1
alejox <alejoxdrums@gmail.com>
2022-06-17 01:03:09 -0500

    InnerText

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-const originalPrice = 150;
-const discount = 15;
+function priceWithDiscount (price, discount){
+  const percentageWithDiscount = 100 - discount;
+  const priceWithDiscount = (price * percentageWithDiscount)/100;
+  return priceWithDiscount
+}
+
+
+function getDiscount (){
+  const inputPrice = document.getElementById('price');
+  const price = inputPrice.value;
+  const inputDiscount = document.getElementById('discount');
+  const discount = inputDiscount.value;
+
+  const priceDiscount = priceWithDiscount(price, discount);
+  
+  const resultP = document.getElementById('resultPrice');
+  resultP.innerText = `El precio con descuento es $${priceDiscount} USD` 
+}
 
 
 
